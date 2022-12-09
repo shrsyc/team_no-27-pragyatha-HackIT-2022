bb27ea78781e6d291d383d01720318a55b7d16c9
shrsyc <shrsyc@gmail.com>
2022-12-10 00:07:25 +0530

    prints max length.

--- SnakeSequenceLength.java
+++ SnakeSequenceLength.java
@@ -1,40 +1,36 @@
 public class SnakeSequenceLength {
 
-    public static int maxSequence(int [][] matrix){
+    public static int maxSequence(int [][] board){
 
-        int rows = matrix.length;
-        int cols = matrix[0].length;
+        int row = board.length , col = board[0].length;
         int maxLenth =1;
         
         
-        int [][] result = new int [rows][cols];
+        int [][] result = new int [row][col];
 
         
-        for (int i = 0; i <rows ; i++) {
-            for (int j = 0; j <cols ; j++) {
+        for (int i = 0; i <row ; i++) {
+            for (int j = 0; j <col ; j++) {
                 result[i][j] =1;
             }
         }
 
-        for (int i = 0; i <rows ; i++) {
-            for (int j = 0; j <cols ; j++) {
+        for (int i = 0; i <row ; i++) {
+            for (int j = 0; j <col ; j++) {
                 if(i!=0 || j!=0){
                     
-                    if(i>0 && Math.abs(matrix[i][j]-matrix[i-1][j])==1){
+                    if(i>0 && Math.abs(board[i][j]-board[i-1][j])==1){
                         result[i][j] = Math.max(result[i][j],result[i-1][j]+1);
                         if(maxLenth<result[i][j]){
                             maxLenth = result[i][j];
-                            
                         }
                     }
 
                     
-                    if(j>0 && Math.abs(matrix[i][j]-matrix[i][j-1])==1){
-                        result[i][j] = Math.max(result[i][j],
-                                result[i][j-1]+1);
+                    if(j>0 && Math.abs(board[i][j]-board[i][j-1])==1){
+                        result[i][j] = Math.max(result[i][j],result[i][j-1]+1);
                         if(maxLenth<result[i][j]){
-                            maxLenth = result[i][j];
-                           
+                            maxLenth = result[i][j];   
                         }
                     }
                 }
@@ -42,7 +38,7 @@ public class SnakeSequenceLength {
         }
 
       
-        System.out.println("Max Snake Sequence : " + maxLenth);
+        System.out.println("Snake Sequence of max length : " + maxLenth);
         
         return 0;
     }
@@ -50,11 +46,11 @@ public class SnakeSequenceLength {
    
 
     public static void main(String[] args) {
-        int arrA [][] =  {{7, 5, 2, 3, 1},
+        int board [][] =  {{7, 5, 2, 3, 1},
                           {3, 4, 1, 4, 4},
                           {1, 5, 6, 7, 8},
                           {3, 4, 5, 8, 9},
                           {3, 2, 2, 7, 6}};
-         maxSequence(arrA);
+         maxSequence(board);
     }
 }

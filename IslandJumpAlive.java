dde749a3a6c2aae8700461ff2b10764c8df51574
shrsyc <shrsyc@gmail.com>
2022-12-10 10:38:06 +0530

    got the probability.

--- IslandJumpAlive.java
+++ IslandJumpAlive.java
@@ -1,19 +1,15 @@
 public class IslandJumpAlive {
-    static double findProbability(int m, int n,int x, int y,int steps)
+    static double getProbability(int m, int n,int x, int y,int steps)
     {
         if (x<0 || y<0 || x>=m || y>=n)return 0.0;
         if (steps == 0)return 1.0;
         
         double probability = 0.0;
     
-        // move up
-        probability += findProbability(m , n , x - 1, y , steps - 1) * 0.25;
-        // move right
-        probability += findProbability(m , n , x , y + 1 , steps - 1) * 0.25;
-        // move down
-        probability += findProbability(m , n , x + 1 , y , steps - 1) * 0.25;
-        // move left
-        probability += findProbability(m , n , x , y - 1 , steps - 1) * 0.25;
+        probability += getProbability(m , n , x - 1, y , steps - 1) * 0.25;
+        probability += getProbability(m , n , x , y + 1 , steps - 1) * 0.25;
+        probability += getProbability(m , n , x + 1 , y , steps - 1) * 0.25;
+        probability += getProbability(m , n , x , y - 1 , steps - 1) * 0.25;
     
         return probability;
     }
@@ -23,7 +19,7 @@ public class IslandJumpAlive {
         int i = 0, j = 0;
         int steps = 1;
     
-        System.out.println("Probability is " +findProbability(m , n , i , j , steps));
+        System.out.println("Probability is " +getProbability(m , n , i , j , steps));
     
     }
  
